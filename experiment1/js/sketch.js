9004c28b7a1177b2ec5c8cbbfba5c7fdf1cc1fd8
corvuscorae <corvuscorae@gmail.com>
2025-04-03 20:48:49 -0700

    generate new scrolls + pause/restart

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -16,21 +16,10 @@ let canvasContainer;
 var centerHorz, centerVert;
 let exampleScroll = "War! The Republic is crumbling under attacks by the ruthless Sith Lord, Count Dooku. There are heroes on both sides. Evil is everywhere. \n\nIn a stunning move, the fiendish droid leader, General Grievous, has swept into the Republic capital and kidnapped Chancellor Palpatine, leader of the Galactic Senate. \n\nAs the Separatist Droid Army attempts to flee the besieged capital with their valuable hostage, two Jedi Knights lead a desperate mission to rescue the captive Chancellor...."
 let scroll = getScroll();
+let pause = false;
+let restart = false;
 
 const DIV_ID = "canvas-container"  // where to put text (see ../index.html)
-
-
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -53,53 +42,59 @@ function setup() {
   canvas.parent(DIV_ID);
   // resize canvas is the page is resized
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
   $(window).resize(function() {
     resizeScreen();
   });
   resizeScreen();
 
-  y = height/2;
-
-  
+  y = height/1.5; 
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    // Star Wars opening crawl by CodeFish via p5js.org! See the original here:
-    //  https://editor.p5js.org/Codefish/sketches/fABwyG9Jz
-    background(0);
-    push();
-    textFont(crawlFont);
-    translate(0, y, z);
-    rotateX(PI/4);
-    textAlign(CENTER, TOP);
-    textSize(width/12);
-    fill(255, 200, 0);
-    //text("Episode III\nREVENGE OF THE SITH",0,0);
-    textSize(width/20);
-    var w = width*0.8;
-    text(scroll, -w/2, width/4, w,height*20);
-    pop();
+  // Star Wars opening crawl by CodeFish via p5js.org! See the original here:
+  //  https://editor.p5js.org/Codefish/sketches/fABwyG9Jz
+  background(0);
+  push();
+  textFont(crawlFont);
+  translate(0, y, z);
+  rotateX(PI/4);
+  textAlign(CENTER, TOP);
+  textSize(width/12);
+  fill(255, 200, 0);
+  // only show text when not restarting (prevents visual bugs)
+  if(!restart) text("Episode III\nREVENGE OF THE SITH",0,0);
+  textSize(width/20);
+  var w = width*0.8;
+  if(!restart) text(scroll, -w/2, width/4, w,height*20);
+  pop();
+
+  if(!pause) {
+    // moves text
     y -= height/800;
     z -= height/800;
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  }
+  if(restart){
+    // move text back to start position
+    y = height/1.5;
+    z = 0;
+    restart = false;
+  }
 }
 
 function getScroll() {
   let scroll = new OpeningScroller();
   return scroll.generate();
-
-  // global box 
-  //$(DIV_ID).text(generate);
 }
 
-// global clicker 
-//$("#clicker").click(main);
+function pauseScroll() {
+  pause = !pause;
+  let symbol = (pause) ? "▶️" : "⏸️"
+  $("#pauser").text(symbol);
+}
+
+// global clickers 
+$("#clicker").click(() => { scroll = getScroll(); restart = true; });
+$("#pauser").click(pauseScroll);
+$("#restarter").click(() => { restart = true });
 
